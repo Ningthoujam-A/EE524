e9b22c7204faefe54aaa7b7f0a9de86dc87207ef
Ningthoujam-A <71975144+Ningthoujam-A@users.noreply.github.com>
2020-10-04 23:03:11 +0530

    Revert "Assignments"

--- Assignment1/204102311_SatyakiGhosh/(3)_factorial.py
+++
@@ -1,9 +0,0 @@
-def fact(n): #recursive funtion to find factorial of a number
-    if n==1:
-        return 1
-    return n*fact(n-1)
-
-
-print('Enter the number:')
-a=int(input())
-print(f'Factorial of {a} = {fact(a)}')
